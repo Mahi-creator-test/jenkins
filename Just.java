3abbba6688495788c875136844b0148b14c2df88
Mahi-creator-test <etester115@gmail.com>
2025-06-27 10:23:32 +0530

    First commit
    first file added

---
+++ Just.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class statement {
+
+    public static void main(String[] args) {
+
+       System.out.println("one");
+       System.out.println("two");
+       System.out.println("three");
+        
+        
+        
+    }
+}
